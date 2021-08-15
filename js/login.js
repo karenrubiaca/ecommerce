1934176bc7c06077dc0ba0c439459f0518d53eae
karenrubiaca <88321581+karenrubiaca@users.noreply.github.com>
2021-08-14 23:25:37 -0300

    logg
    modificacion 23:25

--- js/login.js
+++ js/login.js
@@ -1,6 +1,26 @@
+
+const array = [];
+const login = () => {
+  const name = document.getElementById("txtcorreo").value;
+  const pass = document.getElementById("txtclave").value;
+  if (name && pass) {
+    tbody.innerHTML += `<tr><td>${name}</td><td>${pass}</td></tr>`;
+    //limpiar el texto para que luego se puedan escribir mas
+    // y que no quede registro de lo anterior.
+    document.getElementById("txtcorreo").value = "";
+    document.getElementById("txtclave").value = "";
+    array.push({name: correo,name: clave});
+    window.location="index.html";
+  } else {
+    alert("Correo y Clave no deben ser vacíos");
+  }
+};
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("btnSave").addEventListener("click", buttonSave);
+    
 });
