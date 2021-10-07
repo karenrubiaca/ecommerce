6f3ec539bda2ed83931f5ee270e7054b0628302b
karenrubiaca <88321581+karenrubiaca@users.noreply.github.com>
2021-10-06 23:42:09 -0300

    commit

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,42 @@
+var cart = {};
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    getJSONData(CART).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            let elemCartAdd="";
+          
+            cart=resultObj.data;
+            //articles
+           // console.log(cart);
+            
+           // console.log(cart.articles);
+            for(let indice of cart.articles){
+                let elementCart=indice;
+                elemCartAdd+= `
+                <tr>
+                <th>`+ "Producto" +`</th>
+                <th>`+"Descripción"+`</th>
+                <th>`+"Precio"+`</th>
+                <th>`+"Cantidad de Productos"+`</th>
+                <th>`+"Subtotal"+`</th>
+                <th>`+"Envío"+`</th>
+                <th>`+"Total"+`</th>
+              </tr>
+                <tr>
+                <td><img src="` + elementCart.src + `" alt="` + elementCart.description + `" class="img-thumbnail" style="width:160px; height:160px"></td>
+                <td><h4 class="mb-1">`+ elementCart.name +`</h4></td>
+               
+                <td><p class="price">`+elementCart.currency+" "+elementCart.unitCost+" "+ `</p></td>
+                <td><p class="price">` + elementCart.count +`</p></td>
+               </tr>
+               <hr>                ` 
+                
+            }
+            document.getElementById("addElemCart").innerHTML=elemCartAdd;
+            
+        }
+    
+        });
 });
