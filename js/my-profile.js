1a700eeea274da4242508485455a31a2cf2295fe
karenrubiaca <88321581+karenrubiaca@users.noreply.github.com>
2021-10-25 15:23:43 -0300

    commit

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,28 @@
+$(document).ready(function() {
+
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+});
+
+function guardarDatos(nombresPerfil,apellidosPerfil,edadPerfil,emailPerfil){
+    localStorage
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
